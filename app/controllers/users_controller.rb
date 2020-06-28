3ec077aed5977d17ea3db05ba4edc99f0141ac65
jstloyal <jstloyalty@gmail.com>
2020-06-28 23:36:07 +0100

    complete sign in function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if session[:user_name].nil?
+      @user = User.new
+    else
+      @user = User.find(session[:id])
+      flash[:info] = "You are already signed in"
+      redirect_to @user
+    end
   end
 
   def create
-
+    @user = User.new(user_params)
+    @user.save
+    session[:user_name] = @user.name
+    session[:id] = @user.id
+    cookies[:name] = @user.name
+    cookies[:id] = @user.id
+    redirect_to @user
   end
 
   def show
