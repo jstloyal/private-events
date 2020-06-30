6042a849f4bcb2ef13b11d3775f3829a6a15b142
jstloyal <jstloyalty@gmail.com>
2020-06-30 01:14:34 +0100

    add velidations to event and user model plus write rspec test for event and invitation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :date, :location, :description, presence: true
+
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations, source: :attendee
   belongs_to :creator, class_name: 'User'
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Adetayo') }
+  let(:event) { Event.new(description: 'Birthday', creator_id: 1, date: '2020-21-09') }
+  let(:invitation) { Invitation.new(attendee_id: 1, attended_event_id: 1) }
+
+  describe 'Associations in Invitation' do
+    it 'invitation belongs to attendee' do
+      expect(invitation).to respond_to(:attendee)
+    end
+
+    it 'belongs to attended_event' do
+      expect(invitation).to respond_to(:attended_event)
+    end
+  end
+
+  describe 'Validation of an Invitation' do
+    it 'has uniqueness on columns attendee_id and attended_event_id' do
+      Invitation.create(attendee_id: 1, attended_event_id: 1)
+      expect(invitation).not_to be_valid
+    end
+  end
 end
