7e2c09987d32c7993a2848bc11efc41762d1fbee
jstloyal <jstloyalty@gmail.com>
2020-07-01 15:01:06 +0100

    update index page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,11 +1,23 @@
 module EventsHelper
   def attend_link(event)
-    user = User.find(session[:user_id])
-    invitations = user.invitations.map(&:attended_event_id)
-    if invitations.include?(event.id)
-      link_to 'Unattend event', invitation_path(event), method: :delete, class: 'btn btn-xs btn-primary'
-    else
-      link_to 'Attend event', invitations_path(event), method: :post, class: 'btn btn-xs btn-primary'
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      invitations = user.invitations.map(&:attended_event_id)
+      if invitations.include?(event.id)
+        return link_to 'Unattend event', invitation_path(event), method: :delete, class: 'btn btn-xs btn-primary'
+      else
+        return link_to 'Attend event', invitations_path(event), method: :post, class: 'btn btn-xs btn-primary'
+      end
     end
+    nil
+  end
+
+  def edit_link(event)
+    return link_to 'Edit this event', edit_event_path(event), class: "btn btn-xs btn-primary" if session[:user_id]
+  end
+  
+  def delete_link(event)
+    c = {confirm: "Are you sure you want to delete the event?"}
+    return link_to('Delete this event', event_path(event), method: :delete, data: c, class: "btn btn-xs btn-danger") if session[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'events#index'
   resources :users, only: %i[index new create show]
   resources :events
   resources :sessions, only: %i[new create destroy]
